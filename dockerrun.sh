6cde47c104381a534e7ac9154788419604b435b4
Denis O. Pavlov <pavlovdo@gmail.com>
2016-12-02 12:27:47 +0300

    fixed small bug in dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -13,10 +13,10 @@ then
 	if [ -z $HOST_MOUNT_DIR ]
 	then
 		echo -e '\nRunning of container from image ubuntu:'$PROJECT' with mounting '$PRODUCTION_DIR'/'$PROJECT'/data:'$PRODUCTION_DIR'/'$PROJECT'/data and '$CONFIG_DIR':'$CONFIG_DIR':ro'
-		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v:$CONFIG_DIR:$CONFIG_DIR:ro ubuntu:$PROJECT
+		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $CONFIG_DIR:$CONFIG_DIR:ro ubuntu:$PROJECT
 	else
 		echo -e '\nRunning of container from image ubuntu:'$PROJECT' with mounting '$PRODUCTION_DIR'/'$PROJECT'/data:'$PRODUCTION_DIR'/'$PROJECT'/data and '$HOST_MOUNT_DIR':'$HOST_MOUNT_DIR 'and '$CONFIG_DIR':'$CONFIG_DIR':ro'
-		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $HOST_MOUNT_DIR:$HOST_MOUNT_DIR -v:$CONFIG_DIR:$CONFIG_DIR:ro ubuntu:$PROJECT
+		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $HOST_MOUNT_DIR:$HOST_MOUNT_DIR -v $CONFIG_DIR:$CONFIG_DIR:ro ubuntu:$PROJECT
 	fi
 	rm -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage
 fi
