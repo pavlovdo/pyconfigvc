46e7d098887dac4c35d942e3893a0976b631bbc3
Denis O. Pavlov <pavlovdo@gmail.com>
2016-12-02 13:07:11 +0300

    transfer removing old and running new docker container from dockerrun.sh to build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 PROJECT=`basename $WORKSPACE`
 PRODUCTION_SERVER=eye.forum.lo
 PRODUCTION_DIR=/usr/local/orbit
+CONFIG_DIR=/etc/orbit/$PROJECT
 
 ssh jenkins@$PRODUCTION_SERVER "sudo [ -d $PRODUCTION_DIR/$PROJECT/data ] || sudo mkdir -pv $PRODUCTION_DIR/$PROJECT/data"
 ssh jenkins@$PRODUCTION_SERVER "sudo [ -d $PRODUCTION_DIR/$PROJECT/dockerbuild ] || sudo mkdir -pv $PRODUCTION_DIR/$PROJECT/dockerbuild"
@@ -12,13 +13,11 @@ scp configread.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuil
 scp $PROJECT.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuild
 scp pynetdevices.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuild
 scp pyslack.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuild
+ssh jenkins@$PRODUCTION_SERVER "sudo docker stop `sudo docker ps --filter ancestor=ubuntu:$PROJECT --format "table {{.ID}}" | sed '1,1d'`"
 ssh jenkins@$PRODUCTION_SERVER "sudo docker build -t ubuntu:$PROJECT $PRODUCTION_DIR/$PROJECT/dockerbuild"
-scp dockerrun.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT
+ssh jenkins@$PRODUCTION_SERVER "sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $CONFIG_DIR:$CONFIG_DIR:ro ubuntu:$PROJECT"
 scp outputsend.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT
-ssh jenkins@$PRODUCTION_SERVER "chmod -v u+x $PRODUCTION_DIR/$PROJECT/dockerrun.sh"
 ssh jenkins@$PRODUCTION_SERVER "chmod -v u+x $PRODUCTION_DIR/$PROJECT/outputsend.sh"
-ssh jenkins@$PRODUCTION_SERVER "echo '*/5 * * * *	$PRODUCTION_DIR/$PROJECT/dockerrun.sh' > /tmp/crontab"
 ssh jenkins@$PRODUCTION_SERVER "echo '0 */1 * * *       $PRODUCTION_DIR/$PROJECT/outputsend.sh' >> /tmp/crontab"
 ssh jenkins@$PRODUCTION_SERVER "crontab /tmp/crontab"
 ssh jenkins@$PRODUCTION_SERVER "rm /tmp/crontab"
-ssh jenkins@$PRODUCTION_SERVER "touch $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage"
