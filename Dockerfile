a91e7a2a951d27fba1ff9152e3e0ade5e384e513
Denis O. Pavlov <pavlovdo@gmail.com>
2016-12-05 17:53:35 +0300

    many changes in build.sh and Dockerfile - accordingly dockerfile best practices

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM dr.forum.lo/ubuntu:python3-cron
 MAINTAINER Denis O. Pavlov pavlovdo@gmail.com
 
-RUN apt-get update && apt-get upgrade -y && apt-get install -y \
+RUN apt-get update && apt-get install -y \
 	python3-paramiko \
 	python3-pygit2 \
 	python3-requests
 
-ADD *.py /usr/local/orbit/pyconfigvc/
+COPY *.py /usr/local/orbit/pyconfigvc/
 
 RUN echo "00 04 * * *	/usr/local/orbit/pyconfigvc/pyconfigvc.py > /usr/local/orbit/pyconfigvc/data/output" > /tmp/crontab && \
 	crontab /tmp/crontab && rm /tmp/crontab
-CMD /usr/sbin/cron -f
+
+CMD ["/usr/sbin/cron","-f"]
--- build.sh
+++ build.sh
@@ -8,15 +8,10 @@ ssh jenkins@$PRODUCTION_SERVER "sudo [ -d $PRODUCTION_DIR/$PROJECT/data ] || sud
 ssh jenkins@$PRODUCTION_SERVER "sudo [ -d $PRODUCTION_DIR/$PROJECT/dockerbuild ] || sudo mkdir -pv $PRODUCTION_DIR/$PROJECT/dockerbuild"
 ssh jenkins@$PRODUCTION_SERVER "sudo chown -vR jenkins:jenkins $PRODUCTION_DIR"
 scp Dockerfile jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuild
-scp configread.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuild
-scp $PROJECT.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuild
-scp pynetdevices.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuild
-scp pyslack.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuild
+scp *.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuild
 ssh jenkins@$PRODUCTION_SERVER "sudo docker build -t ubuntu:$PROJECT $PRODUCTION_DIR/$PROJECT/dockerbuild"
-scp dockerrun.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT
-scp outputsend.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT
-ssh jenkins@$PRODUCTION_SERVER "chmod -v u+x $PRODUCTION_DIR/$PROJECT/dockerrun.sh"
-ssh jenkins@$PRODUCTION_SERVER "chmod -v u+x $PRODUCTION_DIR/$PROJECT/outputsend.sh"
+scp *.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT
+ssh jenkins@$PRODUCTION_SERVER "chmod -v u+x $PRODUCTION_DIR/$PROJECT/*.sh"
 ssh jenkins@$PRODUCTION_SERVER "echo '*/5 * * * *	$PRODUCTION_DIR/$PROJECT/dockerrun.sh' > /tmp/crontab"
 ssh jenkins@$PRODUCTION_SERVER "echo '0 */1 * * *       $PRODUCTION_DIR/$PROJECT/outputsend.sh' >> /tmp/crontab"
 ssh jenkins@$PRODUCTION_SERVER "crontab /tmp/crontab"
