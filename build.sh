4db03df7e184a11f2f1a29206fdcb9018ade0533
Denis O. Pavlov <pavlovdo@gmail.com>
2016-12-06 12:11:30 +0300

    changed context build from directory to git

--- build.sh
+++ build.sh
@@ -4,12 +4,9 @@ PROJECT=`basename $WORKSPACE`
 PRODUCTION_SERVER=eye.forum.lo
 PRODUCTION_DIR=/usr/local/orbit
 
-ssh jenkins@$PRODUCTION_SERVER "sudo [ -d $PRODUCTION_DIR/$PROJECT/data ] || sudo mkdir -pv $PRODUCTION_DIR/$PROJECT/data"
-ssh jenkins@$PRODUCTION_SERVER "sudo [ -d $PRODUCTION_DIR/$PROJECT/dockerbuild ] || sudo mkdir -pv $PRODUCTION_DIR/$PROJECT/dockerbuild"
-ssh jenkins@$PRODUCTION_SERVER "sudo chown -vR jenkins:jenkins $PRODUCTION_DIR"
-scp Dockerfile jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuild
-scp *.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockerbuild
-ssh jenkins@$PRODUCTION_SERVER "sudo docker build -t ubuntu:$PROJECT $PRODUCTION_DIR/$PROJECT/dockerbuild"
+ssh jenkins@$PRODUCTION_SERVER "sudo chown -R jenkins:jenkins $PRODUCTION_DIR"
+ssh jenkins@$PRODUCTION_SERVER "[ -d $PRODUCTION_DIR/$PROJECT/data ] || mkdir -p $PRODUCTION_DIR/$PROJECT/data"
+ssh jenkins@$PRODUCTION_SERVER "sudo docker build -t ubuntu:$PROJECT git@dev.forum.lo:it/$PROJECT.git"
 scp *.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT
 ssh jenkins@$PRODUCTION_SERVER "chmod -v u+x $PRODUCTION_DIR/$PROJECT/*.sh"
 ssh jenkins@$PRODUCTION_SERVER "echo '*/5 * * * *	$PRODUCTION_DIR/$PROJECT/dockerrun.sh' > /tmp/crontab"
